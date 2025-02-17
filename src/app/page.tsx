a7bd97b5536277245b85e25fcba916678d672f10
Yura Zhekov <yuriy.j@ezlogz.com>
2025-02-17 19:20:49 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,83 +5,87 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
 import CustomButton from '@/components/CustomButton';
-import { useCarSelector } from '@/hooks/useCarSelector';
+import {useCarSelector} from '@/hooks/useCarSelector';
 import {IMake} from "@/types/typs";
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Home() {
-  const { car, setCar, year, setYear, makes, years, isDisabled, link, isLoading, isError } =
-      useCarSelector();
+    const {car, setCar, year, setYear, makes, years, isDisabled, link, isLoading, isError} =
+        useCarSelector();
 
-  const handleChange = (event: SelectChangeEvent) => setCar(event.target.value as string);
-  const handleChangeYear = (event: SelectChangeEvent) => setYear(Number(event.target.value));
+    const handleChange = (event: SelectChangeEvent) => setCar(event.target.value as string);
+    const handleChangeYear = (event: SelectChangeEvent) => setYear(Number(event.target.value));
 
-  if (isLoading) {
-    return <div>Загрузка...</div>;
-  }
 
-  if (isError) {
-    return <div>Ошибка загрузки данных</div>;
-  }
+    if (isError) {
+        return <div>Error!</div>;
+    }
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-        <Image
-          className="dark:invert"
-          src="/car.webp"
-          alt="car picture"
-          width={300}
-          height={150}
-          priority
-        />
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center">
 
-        <h2 className="text-sky-500 hover:text-sky-800 text-3xl">Car Selector</h2>
+                {isLoading ? (<CircularProgress/>) :
+                    <>
+                        <Image
+                            className="dark:invert"
+                            src="/car.webp"
+                            alt="car picture"
+                            width={300}
+                            height={150}
+                            priority
+                        />
 
-        <div>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="make-select-label">Make</InputLabel>
-            <Select
-              labelId="make-select-label"
-              id="make-select"
-              value={car || ''}
-              onChange={handleChange}
-            >
-              {makes.length > 0 ? (
-                  makes.map((item: IMake) => (
-                      <MenuItem key={item.MakeId} value={item.MakeId}>
-                        {item.MakeName}
-                      </MenuItem>
-                  ))
-              ) : (
-                  <MenuItem disabled>Loading...</MenuItem>
-              )}
-            </Select>
-            <FormHelperText>Select make</FormHelperText>
-          </FormControl>
+                        <h2 className="text-sky-500 hover:text-sky-800 text-3xl">Car Selector</h2>
 
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="year-select-label">Year</InputLabel>
-            <Select
-              labelId="year-select-label"
-              id="year-select"
-              value={year ? String(year) : ''}
-              onChange={handleChangeYear}
-              displayEmpty
-            >
-              {years.map((year) => (
-                <MenuItem key={year} value={year}>
-                  {year}
-                </MenuItem>
-              ))}
-            </Select>
-            <FormHelperText>Select year</FormHelperText>
-          </FormControl>
+                        <div>
+                            <FormControl sx={{m: 1, minWidth: 120}}>
+                                <InputLabel id="make-select-label">Make</InputLabel>
+                                <Select
+                                    labelId="make-select-label"
+                                    id="make-select"
+                                    value={car || ''}
+                                    onChange={handleChange}
+                                >
+                                    {makes.length > 0 ? (
+                                        makes.map((item: IMake) => (
+                                            <MenuItem key={item.MakeId} value={item.MakeId}>
+                                                {item.MakeName}
+                                            </MenuItem>
+                                        ))
+                                    ) : (
+                                        <MenuItem disabled>Loading...</MenuItem>
+                                    )}
+                                </Select>
+                                <FormHelperText>Select make</FormHelperText>
+                            </FormControl>
+
+                            <FormControl sx={{m: 1, minWidth: 120}}>
+                                <InputLabel id="year-select-label">Year</InputLabel>
+                                <Select
+                                    labelId="year-select-label"
+                                    id="year-select"
+                                    value={year ? String(year) : ''}
+                                    onChange={handleChangeYear}
+                                    displayEmpty
+                                >
+                                    {years.map((year) => (
+                                        <MenuItem key={year} value={year}>
+                                            {year}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText>Select year</FormHelperText>
+                            </FormControl>
+                        </div>
+
+                        <CustomButton href={link} disabled={isDisabled} text="Next"/>
+                    </>
+                }
+            </main>
         </div>
-
-        <CustomButton href={link} disabled={isDisabled} text="Next" />
-      </main>
-    </div>
-  );
+    );
 }
